9b39e0378e73e98d294a716d47bd6d02a01beee9
Uzairx123 <uzairbrohi72@gmail.com>
2026-04-29 18:29:13 +0500

    Remove standalone output for Vercel

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone"
+  // output: "standalone"   // ← COMMENT THIS OUT OR REMOVE IT
 };
 
 module.exports = nextConfig;
